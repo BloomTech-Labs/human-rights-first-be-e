1f275b5e62f836e413f4e0854aaa48a3b83c53b6
Steele Helbling <steelehelbling@gmail.com>
2020-10-12 12:59:46 -0400

    exports down now contains all the user table and deleted profile

--- data/migrations/20200625220949_create-profile.js
+++ data/migrations/20200625220949_create-profile.js
@@ -1,40 +1,37 @@
-exports.up = (knex) => {
+exports.up = function (knex) {
   return knex.schema
     .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable('profiles', function (table) {
-      table.string('id').notNullable().unique().primary();
-      table.string('email');
-      table.string('name');
-      table.string('avatarUrl');
-      table.timestamps(true, true);
+    .createTable('incidents', incidents => {
+      incidents.increments('incident_id');
+      incidents.integer('ds_uuid').unique().notNullable();
+      incidents.string('city');
+      incidents.string('state');
+      incidents.float('lat').notNullable();
+      incidents.float('long').notNullable();
+      incidents.string('title').notNullable();
+      incidents.string('desc');
+      incidents.date('date');
     })
-    .createTable('incident', function (incident) {
-      incident.integer('incident_id').notNullable().unique().primary();
-      incident.string('city');
-      incident.string('state');
-      incident.float('lat').notNullable();
-      incident.float('long').notNullable();
-      incident.string('title').notNullable();
-      incident.string('desc');
-      incident.date('date');
-    })
-    .createTable('sources', function (sources) {
-      sources.integer('incident_id').notNullable().unique().primary();
+    .createTable('sources', (sources) => {
+      sources.increments('incident_id').notNullable().unique().primary();
       sources.integer('src_id');
       sources.string('src_url');
       sources.string('src_type');
     })
-    .createTable('incident_type_of_force', function (incident_type_of_force) {
-      // incident_type_of_force.integer('itof_id').notNullable().unique().primary();
+    .createTable('incident_type_of_force', (incident_type_of_force) => {
+      incident_type_of_force.increments('itof_id');
       incident_type_of_force.integer('type_of_force_id').notNullable().unique();
       incident_type_of_force.integer('incident_id').notNullable().unique();
     })
-    .createTable('type_of_force', function (type_of_force) {
-      // type_of_force.integer('type_of_force_id').notNullable().unique().primary();
+    .createTable('type_of_force', (type_of_force) => {
+      type_of_force.increments('type_of_force_id');
       type_of_force.string('type_of_force');
     });
 };
-
-exports.down = (knex) => {
-  return knex.schema.dropTableIfExists('profiles');
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('incidents')
+    .dropTableIfExists('sources')
+    .dropTableIfExists('incident_type_of_force')
+    .dropTableIfExists('type_of_force');
 };
