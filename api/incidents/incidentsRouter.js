6297ca0c8b3ced5827bbea810e6a1413152c9564
Jason Long <jlong5795@gmail.com>
2020-10-14 12:22:20 -0500

    Hot fix to API on deployment

--- api/incidents/incidentsRouter.js
+++ api/incidents/incidentsRouter.js
@@ -1,26 +1,28 @@
-const express = require('express')
-const router = express.Router()
-const Incidents = require('./incidentsModel')
-const { post } = require('../dsService/dsRouter')
-router.get('/incidents', (req, res) => {
-    Incidents.getAllIncidents().then(incidents => {
-        res.json(incidents)
+const express = require('express');
+const router = express.Router();
+const Incidents = require('./incidentsModel');
+const { post } = require('../dsService/dsRouter');
+
+router.get('/showallincidents', (req, res) => {
+  Incidents.getAllIncidents()
+    .then((incidents) => {
+      res.json(incidents);
     })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({message: 'Request Error'})
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: 'Request Error' });
+    });
+});
 
 router.post('/incidents', (req, res) => {
-    Incidents.createIncident(req.body)
-    .then(post => {
-        res.status(201).json(post)
+  Incidents.createIncident(req.body)
+    .then((post) => {
+      res.status(201).json(post);
     })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({message:'Error creating Record'})
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: 'Error creating Record' });
+    });
+});
 
-module.exports = router
+module.exports = router;
